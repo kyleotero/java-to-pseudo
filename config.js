ca825080013475da1f8ed5600c439b754571e70b
Kyle Otero <61243059+kyleotero@users.noreply.github.com>
2022-09-02 23:04:59 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-  webhook:
-    "https://discord.com/api/webhooks/963620461038686218/KFnEHRMjDQ3JuA539UAJD0KkCuuYbgEZFRkAvewUYzJwsQx-ckMZf2EF75x1xygqxeZw",
-  ipapi:
-    "https://ipgeolocation.abstractapi.com/v1/?api_key=159b017772384f868853bf1904ea59d4",
-};
